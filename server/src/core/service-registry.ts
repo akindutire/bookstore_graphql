9bb04f7675e4c38de676d325e5d96bfc674a2121
akindutire <akinsamuel33@gmail.com>
2022-03-19 16:05:29 +0100

    --

--- server/src/core/service-registry.ts
+++ server/src/core/service-registry.ts
@@ -2,9 +2,9 @@ import { asClass, asValue, AwilixContainer, createContainer } from "awilix";
 import { scopePerRequest } from 'awilix-express';
 import {Express} from "express";
 
-export default function serviceRegistry(app: Express, container: AwilixContainer) {
+export default function serviceRegistry(app: Express) {
 
-    container = createContainer()
+    const container = createContainer()
     app.use(scopePerRequest(container))
     
     // container.register({
