9fb662abc5cd8490c3302d416affecbae9ed8294
akindutire <akinsamuel33@gmail.com>
2022-03-19 15:58:27 +0100

    added entity and fixed db connection

---
+++ server/src/entity/User.ts
@@ -0,0 +1,8 @@
+import  {Entity} from 'typeorm'
+
+@Entity
+class Book {
+
+    
+    public id
+}
--- server/src/service/util/database.ts
+++ server/src/service/util/database.ts
@@ -1,4 +1,3 @@
-import { connect } from "mongoose";
 import config from '../../core/config'
 import log from './logger'
 import { createConnection } from 'typeorm'
@@ -6,16 +5,17 @@ import { createConnection } from 'typeorm'
 export default class DatabaseSvc {
     public static async connect() {
         try{
+
             const connection = await createConnection({
                 type: "mongodb",
                 url: config.server.uri,
             })
+            log.info("Connected to DB")
             
-            connect(config.server.uri).then( (res) => {
-                log.info("Connected to DB")
-            } )
+            return Promise.resolve(true)
         } catch (e) {
-            throw e
+            log.error(e)
+            return Promise.reject(false)
         }
     }
 }
