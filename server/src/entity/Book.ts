0091d9b368524c1d6a5a89ceb64847129a9512dc
akindutire <akinsamuel33@gmail.com>
2022-03-20 23:50:53 +0100

    converted Book entity to graphql object type

--- server/src/entity/Book.ts
+++ server/src/entity/Book.ts
@@ -1,31 +1,37 @@
+import { Field, ObjectType } from "type-graphql";
 import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import BookBaseEntity from "./parent/BookBaseEntity";
 import User from "./User";
 
 @Entity('books')
+@ObjectType()
 export default class Book extends BookBaseEntity {
 
     @Column({
         nullable: false,
     })
+    @Field( () => String )
     name: string = ''
 
     @Column({
         nullable: false,
         length: 12
     })
+    @Field( () => String )
     isbn: string = ''
 
     @Column({
         type: 'text',
         nullable: false,
     })
+    @Field( () => String )
     abstract: string = ''
 
     @Column({
         type: 'simple-array',
         nullable: false
     })
+    @Field( () => [String] )
     authors: string[] = []
 
     @ManyToOne(
@@ -36,6 +42,7 @@ export default class Book extends BookBaseEntity {
         }
     )
     @JoinColumn({ name: 'user_id' })
+    @Field( () => User )
     user: User | undefined
 
 }
