d85a984805e8625dda87851ee6abb38711394b27
akindutire <akinsamuel33@gmail.com>
2022-03-21 18:21:30 +0100

    registered jwt service

--- server/src/core/service-registry.ts
+++ server/src/core/service-registry.ts
@@ -1,14 +1,15 @@
-import { asClass, asValue, AwilixContainer, createContainer } from "awilix";
+import { asClass, asValue, AwilixContainer, createContainer, Lifetime } from "awilix";
 import { scopePerRequest } from 'awilix-express';
 import {Express} from "express";
+import JwtSvc from "../service/util/JwtSvc";
 
 export default function serviceRegistry(app: Express) {
 
     const container = createContainer()
     app.use(scopePerRequest(container))
     
-    // container.register({
-
-    // })
+    container.register({
+        jwt : asClass(JwtSvc, {lifetime: Lifetime.SINGLETON})
+    })
    
 }
