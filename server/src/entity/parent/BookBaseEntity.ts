a117b04d431186d771045a00453b9bafcd536927
akindutire <akinsamuel33@gmail.com>
2022-03-23 00:04:37 +0100

    added listener

--- server/src/entity/parent/BookBaseEntity.ts
+++ server/src/entity/parent/BookBaseEntity.ts
@@ -1,15 +1,27 @@
-import { Entity, BaseEntity, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, BaseEntity, CreateDateColumn, UpdateDateColumn, ObjectID, ObjectIdColumn, Column, AfterUpdate } from "typeorm";
 
 @Entity()
 export default class BookBaseEntity extends BaseEntity  {
  
-    @PrimaryGeneratedColumn('identity')
-    id: any
+    @ObjectIdColumn()
+    id!: ObjectID
 
     @CreateDateColumn()
     created_at: Date = new Date
 
     @UpdateDateColumn()
     updated_at: Date = new Date
+
+    @Column()
+    version: number = 1
+
+
+    @AfterUpdate()
+    increaseVersion() {
+        if (this.version === undefined)
+            this.version = 1
+        else
+            this.version += 1
+    }
 
 }
