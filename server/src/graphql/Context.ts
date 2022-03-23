37ea241576a5ee18ac090ac438edeb1b0fceedee
akindutire <akinsamuel33@gmail.com>
2022-03-23 23:01:23 +0100

    added payload to context

--- server/src/graphql/Context.ts
+++ server/src/graphql/Context.ts
@@ -2,5 +2,6 @@ import { Request, Response } from "express";
 
 export default interface ContextInf{
     req: Request,
-    res: Response
+    res: Response,
+    payload?: {email: string}
 }
--- server/src/graphql/middleware/isAuth.ts
+++ server/src/graphql/middleware/isAuth.ts
@@ -2,24 +2,34 @@ import { MiddlewareFn } from "type-graphql";
 import JwtSvc from "../../service/util/JwtSvc";
 import ContextInf from "../Context";
 import container from './../../core/service-registry'
-
+import log from './../../service/util/logger'
+ 
 export const isAuth:MiddlewareFn<ContextInf> = ({context}, next) => {
 
-    if(!context.req.header('x_authorization')) {
-        throw new Error("Access key not found in x_authorization header")
-    }
+    try{
 
-    let bearer_token: string = context.req.header('x_authorization')!
-    let token = bearer_token.replace('Bearer', '')!.trim()
-
-    const jwt:JwtSvc = container.resolve('jwt')
-
-    const payload = jwt.getClaims(token)
-
-    if(!payload) {
-        throw new Error("Access token seems invalid, login!")
-    }
-
+        if(!context.req.header('x_authorization')) {
+            throw new Error("Access key not found in x_authorization header")
+        }
     
-    return next()
+        let bearer_token: string = context.req.header('x_authorization')!
+        let token = bearer_token.replace('Bearer', '')!.trim()
+    
+        const jwt:JwtSvc = container.resolve('jwt')
+    
+        const payload = jwt.getClaims(token)
+    
+        if(!payload) {
+            throw new Error("Access token seems invalid, login!")
+        }
+    
+        context.payload = payload as {email:string}
+    
+        return next()
+
+    } catch (err: any) {
+        log.error(err)
+        throw new Error(err.message)
+    }
+    
 }
